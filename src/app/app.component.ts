e51080dd10267133ba29e800b9d6284836ea6df7
Pankaj Phour <pankaj.phour70@gmail.com>
2023-06-08 10:43:14 +0530

    basic functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'base64toVideo';
+  form:FormGroup;
+  convertor:boolean = false;
+  media:boolean = false;
+  constructor(private _fb:FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    console.log("Hello from app component");
+    
+      this.form = this._fb.group({
+        input : new FormControl('',Validators.required)
+      })
+      this.form.valueChanges.subscribe((val:any)=>{
+        // console.log(val);
+        if(val.input.length>0){
+          this.convertor = true
+        }
+        else{
+          this.convertor = false;
+          this.media = false;
+        }
+        
+      })
+  }
+
+  convert(){
+    this.media = true;
+  }
+
+  
 }
